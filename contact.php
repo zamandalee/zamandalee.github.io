d56fcbd78c6a18cee3f3aff6bc63ce9cc930eab8
Amanda <a@zalee.me>
2018-09-10 10:14:58 -0700

    Add contact information, no working form yet

---
+++ contact.php
@@ -0,0 +1,15 @@
+<?php
+
+if($_POST["submit"]) {
+    $recipient="a@zalee.me";
+    $subject=$_POST["subject"];
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+}
+
+?>
